b42b28e0896cd0c34b988413cd3cc9de56120b25
chweez <chweyaelv@gmail.com>
2018-01-05 07:47:51 +0300

    Initial commit

---
+++ africastalking.go
@@ -0,0 +1,88 @@
+package africastalking
+
+import "net/http"
+import "encoding/json"
+import "fmt"
+import "bytes"
+
+var responseCode int
+
+const debug = false
+const contentJSON = "application/json"
+
+// Gateway is a Gateway
+type Gateway struct {
+	username    string
+	apiKey      string
+	environment string
+}
+
+// SMS is an sms
+type SMS struct {
+	username   string
+	recipients string
+	message    string
+}
+
+// SMSResponse is a response
+type SMSResponse struct {
+	SMSMessageData SMSMessageData
+}
+
+// SMSMessageData is a SMSMessageData
+type SMSMessageData struct {
+	Recipients []string
+	Message    string
+}
+
+// NewGateway creates a new instance of Gateway and return it or an error
+func NewGateway(username, apiKey, environment string) (*Gateway, error) {
+	return &Gateway{
+		username,
+		apiKey,
+		environment,
+	}, nil
+}
+
+// SendSms sends an sms
+func (gateway Gateway) SendSms(recipients, message string) ([]string, error) {
+	sms := SMS{
+		gateway.username,
+		recipients,
+		message}
+	return gateway.sendSms(sms)
+}
+
+func (gateway Gateway) sendSms(sms SMS) ([]string, error) {
+	body, err := json.Marshal(sms)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := http.Post(gateway.getSmsURL(), contentJSON, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	var smsResponse SMSResponse
+	json.NewDecoder(response.Body).Decode(&smsResponse)
+	defer response.Body.Close()
+
+	if len(smsResponse.SMSMessageData.Recipients) > 0 {
+		return smsResponse.SMSMessageData.Recipients, nil
+	}
+
+	return nil, fmt.Errorf("could not send sms message: %s", smsResponse.SMSMessageData.Message)
+}
+
+func (gateway Gateway) getAPIHost() string {
+	if gateway.environment == "sandbox" {
+		return "https://api.sandbox.africastalking.com"
+	}
+
+	return "https://api.africastalking.com"
+}
+
+func (gateway Gateway) getSmsURL() string {
+	return gateway.getAPIHost() + "/version1/messaging"
+}

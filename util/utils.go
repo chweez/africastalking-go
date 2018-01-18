b67fd23100a49cb877e1b269fa0595ac622fbb2d
chweez <chweyaelv@gmail.com>
2018-01-18 14:01:07 +0300

    correct urls

--- util/utils.go
+++ util/utils.go
@@ -25,3 +25,45 @@ func GetAPIHost(env string) string {
 func GetSmsURL(env string) string {
 	return GetAPIHost(env) + "/version1/messaging"
 }
+
+func GetPaymentHost(env string) string {
+	if env == "sandbox" {
+		return "https://payments.sandbox.africastalking.com"
+	}
+	return "https://payments.africastalking.com"
+}
+
+func GetVoiceHost(env string) string {
+	if env == "sandbox" {
+		return "https://voice.sandbox.africastalking.com"
+	}
+	return "https://voice.africastalking.com"
+}
+
+func GetVoiceUrl(env string) string {
+	return GetVoiceHost(env)
+}
+
+func GetSubscriptionUrl(env string) string {
+	return GetAPIHost(env) + "/version1/subscription"
+}
+
+func GetUserDataUrl(env string) string {
+	return GetAPIHost(env) + "/version1/user"
+}
+
+func GetAirtimeUrl(env string) string {
+	return GetAPIHost(env) + "/version1/airtime"
+}
+
+func GetMobilePaymentCheckoutUrl(env string) string {
+	return GetPaymentHost(env) + "/mobile/checkout/request"
+}
+
+func GetMobilePaymentB2BUrl(env string) string {
+	return GetPaymentHost(env) + "/mobile/b2b/request"
+}
+
+func GetMobilePaymentB2CUrl(env string) string {
+	return GetPaymentHost(env) + "/mobile/b2c/request"
+}
